4dd3a739ddd3f00010c2ce2d8ab43165d578b3af
loydcercenia-Paul <loydcercenia@gmail.com>
2025-08-28 07:45:24 +0800

    Add Hardhat configuration for SKALE network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     skale: {
-      url: process.env.SKALE_RPC,
-      accounts: [process.env.DEPLOYER_KEY],
+      url: process.env.SKALE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
